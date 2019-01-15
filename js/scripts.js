16fde0b0cb55ecfb13479dbd4b7881acffd86819
Randee Layosa and Ryan McLean <ryan@>
2019-01-15 15:04:46 -0800

    added flip for the flash cards. added tons more text, classes, ids

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,48 @@
 $(document).ready(function() {
+  $("#js1").click(function() {
+    $("#js").slideToggle("slow");
+    $(".card").flip();
+  });
+
+  $("#operators1").click(function() {
+    $("#operators").slideToggle("slow");
+    $(".card").flip();
+  });
+
+  $("#variables1").click(function() {
+    $("#variables").slideToggle("slow");
+    $(".card").flip();
+  });
+
+  $("#varname1").click(function() {
+    $("#varname").slideToggle("slow");
+    $(".card").flip();
+  });
+
+  $("#functions1").click(function() {
+    $("#functions").slideToggle("slow");
+    $(".card").flip();
+  });
+
+  $("#methods1").click(function() {
+    $("#methods").slideToggle("slow");
+    $(".card").flip();
+  });
+
+  $("#arguments1").click(function() {
+    $("#arguments").slideToggle("slow");
+    $(".card").flip();
+  });
+
+  $("#parameters1").click(function() {
+    $("#parameters").slideToggle("slow");
+    $(".card").flip();
+  });
+
+  $("#return1").click(function() {
+    $("#return").slideToggle("slow");
+    $(".card").flip();
+  });
+
 
 });
